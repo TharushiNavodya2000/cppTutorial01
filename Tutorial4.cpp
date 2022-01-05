b60ce9b9fd419d0334d5eee7866e463fb5ba0edb
Tharushi Navodya <87310863+TharushiNavodya2000@users.noreply.github.com>
2022-01-05 00:36:30 +0000

    T4

---
+++ Tutorial4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "=================================="<<endl
+		<< "\= Student Information =" << endl
+		<< "=================================="<<endl
+		<< "= Name : ARDP Ranasignhe         ="
+		<< endl
+		<< "= Reg No : DIS\\08\\M4\\1234        =" 
+		<< endl
+		<< "=  Address : Malabe              ="
+		<< endl << "==================================" << endl;
+
+}
